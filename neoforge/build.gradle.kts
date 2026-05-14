9bded51abf42ec01297e27c08d16fc8ed854a20b
techno-sam <77073745+techno-sam@users.noreply.github.com>
2026-05-14 19:44:00 +0200

    Skybox XI
    neoforge iris compat

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -24,16 +24,16 @@ dependencies {
 
 	modCompileOnly(libs.ffapi.renderer) // only for sodium compat
 	modCompileOnly(libs.sodium.neoforge)
-	if (!inCI && "enable_sodium"().toBoolean()) { // this doesn't work, because of jar-in-jar
+	if (!inCI && "enable_sodium"().toBoolean()) { // this doesn't work, because of jar-in-jar. Sodium must be placed in the mods folder
 		modLocalRuntime(libs.sodium.neoforge)
 	}
 
 	modCompileOnly(libs.iris.neoforge)
-	if (!inCI && "enable_iris"().toBoolean()) { // this doesn't work, because of jar-in-jar
+	if (!inCI && "enable_iris"().toBoolean()) { // this doesn't work, because of jar-in-jar. Iris must be placed in the mods folder
 		modLocalRuntime(libs.iris.neoforge)
-		modLocalRuntime("org.antlr:antlr4-runtime:4.13.1")
+		/*modLocalRuntime("org.antlr:antlr4-runtime:4.13.1")
 		modLocalRuntime("io.github.douira:glsl-transformer:2.0.1")
-		modLocalRuntime("org.anarres:jcpp:1.4.14")
+		modLocalRuntime("org.anarres:jcpp:1.4.14")*/
 	}
 
 	compileOnly(annotationProcessor(libs.mixinextras.common.get())!!)!!
